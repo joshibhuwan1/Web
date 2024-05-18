deefdafaee73491a76305424baa099db181ca852
joshibhuwan1 <bhuwanjoc2006@gmail.com>
2024-05-18 11:39:04 +0545

    Update main.js

--- main.js
+++ main.js
@@ -13,5 +13,5 @@ setInterval(updateClock, 1000);
 
 
 document.getElementById("notesButton").addEventListener("click", function() {
-  window.location.href = "https://panerusagar.github.io/studyhub";
+  window.location.href = "https://joshibhuwan1.github.io/notes";
 });
